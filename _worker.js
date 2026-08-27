d78fad26328b6c803581ea9111415496473a78c5
angeljuice1 <50046987+angeljuice1@users.noreply.github.com>
2026-08-27 10:27:22 -0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,4 +1,3 @@
-export default {
   async fetch(request, env) {
 
     const url = new URL(request.url);
@@ -22,44 +21,3 @@ export default {
 
       return new Response(
         JSON.stringify({
-          success: true,
-          filename: filename
-        }),
-        {
-          headers: {
-            "Content-Type": "application/json"
-          }
-        }
-      );
-    }
-
-    // Retrieve a stored file
-    if (request.method === "GET" && url.pathname.startsWith("/media/")) {
-
-      const filename = decodeURIComponent(
-        url.pathname.replace("/media/", "")
-      );
-
-      const object = await env.VIDEOS.get(filename);
-
-      if (!object) {
-        return new Response("File not found", {
-          status: 404
-        });
-      }
-
-      const headers = new Headers();
-
-      object.writeHttpMetadata(headers);
-      headers.set("etag", object.httpEtag);
-
-      return new Response(object.body, {
-        headers: headers
-      });
-    }
-
-    return new Response("Angelo Media", {
-      status: 200
-    });
-  }
-};
